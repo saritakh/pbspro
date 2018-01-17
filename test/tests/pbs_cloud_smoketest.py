e20a49f3e7fbc491d06d3c9e59e5bc9a65cd43b6
saritakh <sarita.harapanahalli@altair.com>
2018-01-17 15:21:25 +0530

    changes to cloud smoke test - 4

--- test/tests/pbs_cloud_smoketest.py
+++ test/tests/pbs_cloud_smoketest.py
@@ -1,84 +1,87 @@
-# coding: utf-8
-
-# Copyright (C) 1994-2017 Altair Engineering, Inc.
-# For more information, contact Altair at www.altair.com.
-#
-# This file is part of the PBS Professional ("PBS Pro") software.
-#
-# Open Source License Information:
-#
-# PBS Pro is free software. You can redistribute it and/or modify it under the
-# terms of the GNU Affero General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# PBS Pro is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Commercial License Information:
-#
-# The PBS Pro software is licensed under the terms of the GNU Affero General
-# Public License agreement ("AGPL"), except where a separate commercial license
-# agreement for PBS Pro version 14 or later has been executed in writing with
-# Altair.
-#
-# Altair’s dual-license business model allows companies, individuals, and
-# organizations to create proprietary derivative works of PBS Pro and
-# distribute them - whether embedded or bundled with other software - under
-# a commercial license agreement.
-#
-# Use of Altair’s trademarks, including but not limited to "PBS™",
-# "PBS Professional®", and "PBS Pro™" and Altair’s logos is subject to Altair's
-# trademark licensing policies.
 
 from ptl.utils.pbs_testsuite import *
 
-#CLOUD = 'test-cloud-'
-CLOUD = 'x19'
+CLOUD = 'test-cloud-'
+#CLOUD = 'x19'
 
 @tags('smoke')
-class CloudSmokeTest(PBSTestSuite):
+class CloudJobsTest(PBSTestSuite):
 
     """
     This test suite contains a few smoke tests of PBS
 
     """
 
+    @timeout(1000)
     def test_submit_cloud_job(self):
         """
-        Test to submit a job
+        Test to submit a job for cloud burst
         """
-
+        #Workaround for increasing server log level
         a = {'log_events': 2047}
         self.server.manager(MGR_CMD_SET, SERVER, a, expect=True)
 
+        #Workaround for adding node_location to sched_config resources line
         self.scheduler.add_resource('node_location',apply=True)
+
+        #Workaround for adding workq default node_location=local
         a = {'resources_default.node_location': 'local'}
         self.server.manager(MGR_CMD_SET, QUEUE, a, id='workq')
 
-        j1 = Job(TEST_USER)
-        j2 = Job('centos',
-                 attrs={'queue': 'cloudq3',
-                        'Resource_List.select': '1:ncpus=1'})
-        j2.set_sleep_time(1000)
+        #Get list of Nodes that have ncpus > 0
+        a = {'state':'free'}
+        free_nodes = self.server.filter(NODE, a)
+        nodes = free_nodes.values()[0]
+        self.logger.info('skh - nodes are ' + str(nodes))
 
+        #For every local node submit a workq job
+        for n in nodes:
+            j = Job('centos',
+                    attrs={'Resource_List.select':'1:node_location=local',
+                           'Resource_List.place': 'exclhost'})
+            j.set_sleep_time(1000)
+            jid = self.server.submit(j)
+            self.server.expect(JOB, {'job_state': 'R'}, id=jid)
 
+        #Submit a cloud job to cloudq3
         j3 = Job('centos',
                  attrs={'queue': 'cloudq3',
                         'Resource_List.select': '1:ncpus=1'})
-
-        #jid1 = self.server.submit(j1)
-        jid2 = self.server.submit(j2)
+        j3.set_sleep_time(100)
         jid3 = self.server.submit(j3)
 
-        #self.server.expect(JOB, {'job_state': 'R'}, id=jid1)
-        self.server.expect(JOB, {'job_state': 'R'}, id=jid2)
+        #Expect running state of cloud job
         self.server.expect(JOB, {'job_state': 'R'}, id=jid3,
-                           max_attempts=60,interval=10)
+                            max_attempts=60,interval=10)
 
+        #Ensure exec_vnode of cloud job is a cloud node
         self.server.expect(JOB, {'exec_vnode': (MATCH,CLOUD)}, id=jid3)
+
+        #Get list of Nodes that have ncpus > 0
+        a = {'resources_available.ncpus': (GT, 0)}
+        free_nodes = self.server.filter(NODE, a)
+        nodes = free_nodes.values()[0]
+        self.logger.info('skh - nodes after burst are ' + str(nodes))
+
+        #Wait for cloud job to finish
+        self.server.log_match(
+            jid3 + ";Exit_status", max_attempts=30, interval=50)
+
+        #Get list of Nodes that have ncpus > 0
+        a = {'resources_available.ncpus': (GT, 0)}
+        free_nodes = self.server.filter(NODE, a)
+        nodes = free_nodes.values()[0]
+        self.logger.info('skh - nodes after cloud job are ' + str(nodes))
+
+        #Wait time for cloud node to unburst
+        time.sleep(300)
+
+        #Get list of Nodes that have ncpus > 0
+        a = {'resources_available.ncpus': (GT, 0)}
+        free_nodes = self.server.filter(NODE, a)
+        nodes = free_nodes.values()[0]
+        self.logger.info('skh - nodes after unburst are ' + str(nodes))
+
+        if CLOUD not in str(nodes):
+            self.logger.info('skh - cloud nodes unburst done')
+
